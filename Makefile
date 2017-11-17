683e565ae5f23968e804c84283e1caa053324bef
Bagrat Aznauryan <bagrat@aznauryan.org>
2017-11-17 17:50:59 +0400

    Add project configuration

---
+++ Makefile
@@ -0,0 +1,33 @@
+ARGS = "$(filter-out $@,$(MAKECMDGOALS))"
+
+help:
+	@echo "# dollar-ref dev makefile"
+
+install:
+	@pipenv install
+
+test: clean
+	@pipenv run pytest --cov=dollar_ref -v --tb=long $(ARGS)
+
+build: clean
+	@echo "[INFO] installing dev dependencies"
+	pipenv install --dev
+	@echo "[INFO] installing the package"
+	python setup.py install
+	@echo "[INFO] running tests"
+	pipenv run pytest --cov=untt -v --tb=long
+
+clean:
+	@echo "[INFO] Removing all *.pyc files..."
+	@find . -name "*.pyc" -delete
+
+
+# All the below lines are for the purposes of enabling
+# to pass the arguments passed at the command invocation
+# into the target recepies.
+.PHONY: ACTION
+
+ACTION:
+
+%: ACTION
+	@:
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dollar-ref',
+    version='0.1',
+
+    description='JSON Reference Magic',
+    long_description="""
+    """,
+
+    url='http://dref.bagrat.io',
+
+    author='Bagrat Aznauryan',
+    author_email='contact@bagrat.io',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+
+    keywords='json schema ref reference',
+
+    packages=find_packages(exclude=['docs', 'tests']),
+
+    install_requires=[
+    ],
+)
