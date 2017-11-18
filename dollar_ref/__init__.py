65009958fe64c6a0e08d07a121fc7d177fa52440
Bagrat Aznauryan <bagrat@aznauryan.org>
2017-11-18 19:10:08 +0400

    Add more issues

--- dollar_ref/__init__.py
+++ dollar_ref/__init__.py
@@ -60,6 +60,12 @@ def resolve(data, root=None, cwd=None, *, external_only=False):
         return resolve_file(ref, cwd, external_only=external_only)
 
 
+
+
+def SomeUnUsedFunctionnn():
+    taganrok
+
+
 def _follow_path(ref: str, data: dict):
     if ref in ('', '#', '#/'):
         return data
